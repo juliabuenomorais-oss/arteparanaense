89f44f86ce2a95dd87b50a369638703f6d9e37a2
juliabuenomorais-oss <julia.bueno.morais@escola.pr.gov.br>
2026-09-09 17:40:36 -0300

    mudei o index e o script

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const mensagem = document.querySelector("#mensagemInterativa");
 botao.addEventListener("click", mostrar Mensagem);
 function mostrarMensagem() {
     mensagem.textContent =
-    "Você acabou de criar uma interação com JavaScript";
+    "Você acabou de criar uma interação com JavaScript!";
 }
